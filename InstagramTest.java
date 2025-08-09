e3020fdb99000e328290643fdcae8499134e1756
shilpaalex-ui <126485255+shilpaalex-ui@users.noreply.github.com>
2025-08-09 15:50:21 +0100

    Initial commit – Instagram Appium automation
    Note: XPath/id not finalized as phone was not connected during development.

---
+++ InstagramTest.java
@@ -0,0 +1,57 @@
+package instagramAutomation;
+
+import org.testng.annotations.Test;
+
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+
+import org.testng.annotations.BeforeTest;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterTest;
+
+public class InstagramTest  {
+	AndroidDriver driver;
+	@Test
+	public void openInstagramTest() throws InterruptedException {
+		Thread.sleep(5000);
+		try {
+			WebElement element = driver.findElementByXPath("//body");
+	            if (element.isDisplayed()) {
+	                System.out.println("Instagram opened successfully!");
+	            }
+	        } catch (Exception e) {
+	            System.out.println("Failed to verify Instagram home screen: " + e.getMessage());
+	        }
+	    }
+  @BeforeTest
+  public void launchingcommands() throws MalformedURLException {
+	  driver = getDriver();
+	  System.out.println("Server connected");
+	  driver.get("https://www.instagram.com/");
+	  }
+ 
+  @AfterTest
+  public void closeapp() throws InterruptedException {
+	  // close the app
+	  Thread.sleep(5000);
+	  driver.close();
+
+}
+  private AndroidDriver getDriver() throws MalformedURLException {
+	  DesiredCapabilities capabilities = new DesiredCapabilities();
+	  capabilities.setCapability("chromedriverExecutable", System.getProperty("user.dir")+"\\drivers\\chromedriver.exe");
+	  capabilities.setCapability(CapabilityType.BROWSER_NAME, "Chrome");
+	  capabilities.setCapability("deviceName", "realme Narzo 20");
+	  capabilities.setCapability("platformName", "Android");
+	  capabilities.setCapability("platformVersion", "11");
+	  capabilities.setCapability("automationName", "UiAutomator2"); 
+      System.out.println("Listening to Appium server...");
+      return new AndroidDriver<>(new URL("http://0.0.0.0:4723/wd/hub"), capabilities); 
+}
+}
